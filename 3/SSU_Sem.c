4a17e3cbb5f367fb1b63e8c49ed0e4bc699e36ce
Donggggg <ptsaturn68@daum.net>
2020-10-31 12:23:13 +0900

    problem 3 completed

--- 3/SSU_Sem.c
+++ 3/SSU_Sem.c
@@ -8,13 +8,16 @@
 #include "SSU_Sem.h"
 
 void SSU_Sem_init(SSU_Sem *s, int value) {
-  
+	s->sem = value;
 }
 
 void SSU_Sem_down(SSU_Sem *s) {
-
+	s->sem--;
+	while(s->sem < 0);
 }
 
 void SSU_Sem_up(SSU_Sem *s) {
-
+	s->sem++;
+	if(s->sem < 0)
+		s->sem = 0;
 }
